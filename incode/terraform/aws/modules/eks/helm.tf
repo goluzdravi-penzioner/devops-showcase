cf67b61f80f2ee3bfe84c398a79d1386ee218159
goluzdravi <goluzdravi@gmail.com>
2024-08-02 16:06:37 +0200

    tf fmt

---
+++ incode/terraform/aws/modules/eks/helm.tf
@@ -0,0 +1,9 @@
+# resource "helm_release" "argocd" {
+#   name = "argocd"
+#   chart = "argocd"
+# }
+
+# resource "helm_release" "external-secrets" {
+#   name = "external-secrets"
+#   chart = "External-secrets"
+# }
